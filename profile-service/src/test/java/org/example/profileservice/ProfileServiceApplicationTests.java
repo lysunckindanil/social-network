3e2a55accc7cdb839d63ebbd0c10f29cf7bbdfd3
Danila <danil.lysunckin@gmail.com>
2025-01-13 14:59:53 +0500

    init profile-service

---
+++ profile-service/src/test/java/org/example/profileservice/ProfileServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package org.example.profileservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
+@SpringBootTest
+class ProfileServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
