8907753907539dc0395e1b0020c800157d9b316b
Danila <danil.lysunckin@gmail.com>
2025-01-13 17:39:05 +0500

    friends service init

---
+++ friends_service/src/main/java/org/example/friends_service/FriendsServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.friends_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FriendsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FriendsServiceApplication.class, args);
+	}
+
+}
---
+++ friends_service/src/test/java/org/example/friends_service/FriendsServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package org.example.friends_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
+@SpringBootTest
+class FriendsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
