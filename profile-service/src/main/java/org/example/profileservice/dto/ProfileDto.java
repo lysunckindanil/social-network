631d7a5942193f1ba47431b6eb506ad95d5db018
Danila <danil.lysunckin@gmail.com>
2025-01-13 20:11:34 +0500

    remove serialization from dto

--- profile-service/src/main/java/org/example/profileservice/dto/ProfileDto.java
+++ profile-service/src/main/java/org/example/profileservice/dto/ProfileDto.java
@@ -1,17 +1,15 @@
 package org.example.profileservice.dto;
 
-import lombok.*;
+import lombok.Builder;
+import lombok.Data;
 import org.example.profileservice.model.Profile;
-
-import java.io.Serializable;
 
 /**
  * DTO for {@link Profile}
  */
 @Builder
-@AllArgsConstructor
-@EqualsAndHashCode
-public class ProfileDto implements Serializable {
+@Data
+public class ProfileDto{
     private final String username;
     private final String email;
     private final String photoUrl;
