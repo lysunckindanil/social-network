a506361586b0bc3cbb9c6c51df93cbf525a3a98c
Danila <danil.lysunckin@gmail.com>
2025-01-14 19:11:22 +0500

    friends-posts-service init

---
+++ friends-posts-service/src/main/java/org/example/friendspostsservice/FriendsPostsServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.friendspostsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FriendsPostsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FriendsPostsServiceApplication.class, args);
+    }
+
+}
