923653c7357eae920393a6cf1ff323c1523231f0
Danila <danil.lysunckin@gmail.com>
2025-01-14 22:07:39 +0500

    edit docker-compose.yml

---
+++ friend-posts-service/src/main/java/org/example/friendpostsservice/FriendPostsServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.friendpostsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FriendPostsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FriendPostsServiceApplication.class, args);
+    }
+
+}
