cc27a61ea7f1256dd58d05b99df6a107858fea8f
Danila <danil.lysunckin@gmail.com>
2025-01-15 18:05:05 +0500

    web-service

--- web-service/src/test/java/org/example/webservice/WebServiceApplicationTests.java
+++ web-service/src/test/java/org/example/webservice/WebServiceApplicationTests.java
@@ -2,8 +2,9 @@ package org.example.webservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+//@SpringBootTest
 class WebServiceApplicationTests {
 
     @Test
