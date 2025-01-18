1f0f82746aff3dc57af3bfd543230a2211a9a002
Danila <danil.lysunckin@gmail.com>
2025-01-18 20:03:31 +0500

    fix kafka listener

--- share-posts-service/src/main/java/org/example/sharepostsservice/repo/PostSubscriberRepository.java
+++ share-posts-service/src/main/java/org/example/sharepostsservice/repo/PostSubscriberRepository.java
@@ -4,8 +4,10 @@ import org.example.sharepostsservice.model.Post;
 import org.example.sharepostsservice.model.PostSubscriber;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface PostSubscriberRepository extends JpaRepository<PostSubscriber, Long> {
+    @Transactional
     void deleteAllByPost(Post post);
 }
