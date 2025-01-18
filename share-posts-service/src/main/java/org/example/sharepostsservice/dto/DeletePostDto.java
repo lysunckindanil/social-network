43b7ea2c3189cb444d06520fee681ce7af16ee3d
Danila <danil.lysunckin@gmail.com>
2025-01-18 19:49:02 +0500

    fix kafka listener

--- share-posts-service/src/main/java/org/example/sharepostsservice/dto/DeletePostDto.java
+++ share-posts-service/src/main/java/org/example/sharepostsservice/dto/DeletePostDto.java
@@ -1,8 +1,12 @@
 package org.example.sharepostsservice.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@AllArgsConstructor
+@NoArgsConstructor
 @Data
 @Builder
 public class DeletePostDto {
