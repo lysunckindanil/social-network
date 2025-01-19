a3cb78f99033622dc2fbf9b23d408c3623b4e6dc
Danila <danil.lysunckin@gmail.com>
2025-01-19 10:45:29 +0500

    web-service, added profiles page

--- web-service/src/main/resources/static/js/profiles.js
+++ web-service/src/main/resources/static/js/profiles.js
@@ -3,13 +3,13 @@ let page = 0;
 function getProfiles() {
     const token = $("meta[name='_csrf']").attr("content");
     $.ajax({
-        url: '/posts/getProfilesPageable',
+        url: '/profile',
         headers: {"X-CSRF-TOKEN": token},
         method: 'post',
         dataType: 'json',
         data: {page: page},
         success: function (data) {
-            generatePosts(data)
+            generateProfiles(data)
         }
     });
     page += 1
@@ -17,12 +17,12 @@ function getProfiles() {
 
 function generateProfiles(profiles) {
     const posts_section = document.getElementById("profiles_section");
-    profiles.forEach(function (postEntity) {
+    profiles.forEach(function (profileEntity) {
         const profile = document.getElementById("profile").cloneNode(true)
         profile.hidden = false
-        profile.getElementsByClassName("profileUsername")[0].innerText = postEntity['username']
-        profile.getElementsByClassName("profileUsername")[0].href = '/profile/' + postEntity['username']
-        profile.getElementsByClassName("profileEmail")[0].innerText = postEntity['email']
+        profile.getElementsByClassName("profileUsername")[0].innerText = profileEntity['username']
+        profile.getElementsByClassName("profileUsername")[0].href = '/profile/' + profileEntity['username']
+        profile.getElementsByClassName("profileEmail")[0].innerText = profileEntity['email']
         posts_section.insertAdjacentElement('beforeend', profile)
     })
 
