3761f60b468a210d5fa13076a24522409ad4fcd9
Danila <danil.lysunckin@gmail.com>
2025-01-28 14:26:43 +0500

    streamable for repo method

--- subscriber-post-service/src/main/java/org/example/subscriberpostsservice/repo/PostSubscriberRepository.java
+++ subscriber-post-service/src/main/java/org/example/subscriberpostsservice/repo/PostSubscriberRepository.java
@@ -6,12 +6,13 @@ import org.example.subscriberpostsservice.model.Profile;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.util.Streamable;
 
 import java.util.List;
 
 public interface PostSubscriberRepository extends JpaRepository<PostSubscriber, PostSubscriber.Id> {
     @Query("select p.post from PostSubscriber p join fetch p.post.author where p.subscriber=:subscriber")
-    List<Post> findPostsBySubscriber(Profile subscriber);
+    Streamable<Post> findPostsBySubscriber(Profile subscriber);
 
     @Query("select p.post from PostSubscriber p join fetch p.post.author where p.subscriber=:subscriber order by p.post.createdAt desc")
     List<Post> findPostsBySubscriberPageableOrderByCreatedAtDesc(Profile subscriber, Pageable pageable);
