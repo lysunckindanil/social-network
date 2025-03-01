2644f66c7e30c475a539a772a1fe56d14f3a1249
Danila <danil.lysunckin@gmail.com>
2025-03-01 11:15:04 +0500

    switch to http sessions instead of jwt

--- web-service/src/main/java/org/example/webservice/config/SecurityConfig.java
+++ web-service/src/main/java/org/example/webservice/config/SecurityConfig.java
@@ -1,49 +1,28 @@
 package org.example.webservice.config;
 
 import lombok.RequiredArgsConstructor;
-import org.example.webservice.service.security.CookieAuthenticationProvider;
-import org.example.webservice.service.security.filter.CookieAuthenticationFilter;
-import org.example.webservice.service.security.filter.CsrfLoggerFilter;
-import org.example.webservice.service.security.handler.CookieAuthenticationEntryPoint;
 import org.example.webservice.service.security.handler.CustomAccessDeniedHandler;
-import org.example.webservice.service.security.handler.TokenCookieLoginSuccessHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
-import org.springframework.security.web.csrf.CsrfFilter;
-import org.springframework.security.web.savedrequest.RequestCacheAwareFilter;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
-    private final CookieAuthenticationFilter cookieAuthenticationFilter;
-    private final TokenCookieLoginSuccessHandler tokenCookieLoginSuccessHandler;
-    private final AuthenticationConfiguration authenticationConfiguration;
-    private final UserDetailsService userDetailsService;
     private final PasswordEncoder passwordEncoder;
-    private final CookieAuthenticationProvider cookieAuthenticationProvider;
-    private final CookieAuthenticationEntryPoint cookieAuthenticationEntryPoint;
+    private final UserDetailsService userDetailsService;
 
-    @Bean
-    public WebSecurityCustomizer webSecurityCustomizer() {
-        return web -> web
-                .ignoring()
-                .requestMatchers(HttpMethod.GET, "/js/**", "/css/**");
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
     }
 
     @Bean
@@ -51,49 +30,28 @@ public class SecurityConfig {
         http
                 .exceptionHandling((exception)
                         -> exception
-                        .authenticationEntryPoint(cookieAuthenticationEntryPoint)
                         .accessDeniedHandler(new CustomAccessDeniedHandler()))
-                .csrf(csrf -> csrf
-                        //todo consider proper csrf token
+//                .csrf(csrf -> csrf
+                //todo consider proper csrf token
 //                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
 //                        .csrfTokenRequestHandler(new CsrfTokenRequestAttributeHandler())
-                        .sessionAuthenticationStrategy(
-                                ((authentication, request, response) -> {
-                                })))
-                .addFilterAfter(new CsrfLoggerFilter(), CsrfFilter.class)
-                .addFilterBefore(usernamePasswordAuthenticationFilter(), RequestCacheAwareFilter.class)
-                .addFilterBefore(cookieAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
-                .sessionManagement(session ->
-                        session
-                                .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+//                        .addFilterAfter(new CsrfLoggerFilter(), CsrfFilter.class)
                 .authorizeHttpRequests(authorize ->
                         authorize.requestMatchers("/profile/register", "/profile/login").permitAll())
                 .authorizeHttpRequests(authorize ->
                         authorize.requestMatchers("/css/**", "/js/**").permitAll())
                 .authorizeHttpRequests(authorize ->
-                        authorize.anyRequest().hasRole("USER"));
+                        authorize.anyRequest().authenticated())
+                .formLogin(login -> login
+                        .loginPage("/profile/login")
+                        .loginProcessingUrl("/profile/login")
+                        .defaultSuccessUrl("/home")
+                        .failureUrl("/profile/login?error")
+                )
+                .httpBasic(Customizer.withDefaults())
+                .logout(logout -> logout
+                        .logoutUrl("/profile/logout")
+                        .logoutSuccessUrl("/profile/login"));
         return http.build();
-    }
-
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) {
-        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
-        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder);
-        daoAuthenticationProvider.setUserDetailsService(userDetailsService);
-        auth
-                .authenticationProvider(daoAuthenticationProvider)
-                .authenticationProvider(cookieAuthenticationProvider);
-    }
-
-    @Bean
-    public UsernamePasswordAuthenticationFilter usernamePasswordAuthenticationFilter() throws Exception {
-        UsernamePasswordAuthenticationFilter usernamePasswordAuthenticationFilter =
-                new UsernamePasswordAuthenticationFilter(authenticationConfiguration.getAuthenticationManager());
-        usernamePasswordAuthenticationFilter.setAuthenticationSuccessHandler(tokenCookieLoginSuccessHandler);
-        usernamePasswordAuthenticationFilter.setAuthenticationFailureHandler(
-                (request, response, e)
-                        -> response.sendRedirect("/profile/login?error=true"));
-        usernamePasswordAuthenticationFilter.setRequiresAuthenticationRequestMatcher(new AntPathRequestMatcher("/profile/login", "POST"));
-        return usernamePasswordAuthenticationFilter;
     }
 }
--- web-service/src/main/java/org/example/webservice/controller/ProfileController.java
+++ web-service/src/main/java/org/example/webservice/controller/ProfileController.java
@@ -1,16 +1,11 @@
 package org.example.webservice.controller;
 
-import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.example.webservice.dto.ProfileDto;
 import org.example.webservice.model.Profile;
 import org.example.webservice.service.ProfileService;
-import org.example.webservice.service.SubscriberService;
-import org.example.webservice.service.security.CookieService;
 import org.example.webservice.service.security.ProfileSecurityService;
 import org.example.webservice.util.ProfileValidator;
 import org.springframework.stereotype.Controller;
@@ -30,7 +25,6 @@ public class ProfileController {
     private final ProfileService profileService;
     private final ProfileSecurityService profileSecurityService;
     private final ProfileValidator profileValidator;
-    private final CookieService cookieService;
 
     @GetMapping
     public String index(Model model, Principal principal) {
@@ -78,7 +72,7 @@ public class ProfileController {
     }
 
     @PostMapping("/register")
-    public String createUserPost(@ModelAttribute @Valid Profile profile, BindingResult bindingResult, HttpServletResponse response) {
+    public String createUserPost(@ModelAttribute @Valid Profile profile, BindingResult bindingResult) {
         profileValidator.validate(profile, bindingResult);
         if (bindingResult.hasErrors()) {
             return "security/register";
@@ -88,17 +82,7 @@ public class ProfileController {
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
-        Cookie cookie = cookieService.buildCookie(profile.getUsername());
-        response.addCookie(cookie);
-        return "redirect:/";
+        return "redirect:/home";
     }
 
-    @PostMapping("/logout")
-    public String logout(HttpServletRequest request, HttpServletResponse response) {
-        Optional<String> token = cookieService.extractToken(request.getCookies());
-        if (token.isPresent()) {
-            response.addCookie(cookieService.buildLogoutCookie(token.get()));
-        }
-        return "redirect:/profile/login";
-    }
 }
